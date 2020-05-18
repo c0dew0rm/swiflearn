d6a1819192e99f94f2c0847688f9ce67cb56b5a2
rahul110 <rahul@transpacks.co>
2020-05-18 19:24:28 +0530

    Defining models and admin registration

--- mysite/assignment/admin.py
+++ mysite/assignment/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
-
+from .models import UserProfile, Class, Question, ClassTaken
 # Register your models here.
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    fields = ['user', 'age', 'city', 'grade', 'board', ]
+    list_display = ['user', 'age', 'city', 'grade', 'board', ]
+
+
+@admin.register(Class)
+class ClassAdmin(admin.ModelAdmin):
+    fields = ['classTitle', 'instructorName', ]
+    list_display = ['classTitle', 'instructorName', ]
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['className', 'question', ]
+    readonly_fields = ['id',]
+    list_display = ['id', 'className', 'question', ]
+
+
+@admin.register(ClassTaken)
+class ClassTakenAdmin(admin.ModelAdmin):
+    readonly_fields = ['id',]
+    fields = ['user', 'classTaken']
+    list_display = ['id', 'user', 'classTaken']
--- mysite/assignment/models.py
+++ mysite/assignment/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.IntegerField()
+    city = models.CharField(max_length=30, null=False, default=None)
+    grade = models.CharField(max_length=30, null=False, default=None)
+    board = models.CharField(max_length=30, null=False, default=None)
+
+    def __str__(self):
+        return user.username
+
+class Class(models.Model):
+    classTitle = models.CharField(max_length=30, null=False, default=None, unique=True)
+    instructorName = models.CharField(max_length=30, null=False, default=None)
+
+    def __str__(self):
+        return self.classTitle
+
+
+class ClassTaken(models.Model):
+    user = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+    classTaken = models.OneToOneField(Class, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.id
+
+class Question(models.Model):
+    className = models.ForeignKey(Class, on_delete=models.CASCADE)
+    question = models.CharField(max_length=30, null=False, default=None, unique=True)
+
+    def __str__(self):
+        return self.id
