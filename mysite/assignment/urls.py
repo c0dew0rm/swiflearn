2c638cc7c810794fccdb0cad198fa9aad965aa34
rahul110 <rahul@transpacks.co>
2020-05-20 12:34:53 +0530

    adding login and rest urls

--- mysite/assignment/urls.py
+++ mysite/assignment/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from assignment import views
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('index', views.index , name='index'),
-    path('login/', views.login, name='login'),
+    # url(r'^login/$', auth_views.LoginView, name='login'),
+    # url(r'^logout/$', auth_views.LogoutView, name='logout'),
     url(r'^signup/$', views.signup, name='signup'),
+    url(r'^login/$', views.login, name='login'),
 ]
--- mysite/assignment/views.py
+++ mysite/assignment/views.py
@@ -4,6 +4,11 @@ from django.db import transaction
 from django.contrib.auth import login, authenticate
 
 from .forms import UserForm, UserProfileForm
+from .models import UserProfile
+
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
 
 # Create your views here.
 
@@ -30,6 +35,25 @@ def signup(request):
     return render(request, 'signup.html', {'form': user_form, 'form2': profile_form})
 
 
-# @api_view(['POST','GET'])
-# def login(request):
-#     return render(request, 'login.html')
+@api_view(['POST','GET'])
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        try:
+            user = authenticate(username=username,password=password)
+            student = UserProfile.objects.get(user__username=username)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        if student is not None:
+            try:
+                url = "details/"+str(student.id)
+                return redirect(url)
+            except:
+                return Response(status=status.HTTP_404_NOT_FOUND)
+        else:
+            return render("/")
+
+    else:
+        return render(request, 'login.html')
