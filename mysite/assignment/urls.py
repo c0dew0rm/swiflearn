7324bbbed83a1357cb4798b4b22ffb1e6d424312
rahul110 <rahul@transpacks.co>
2020-05-20 15:23:26 +0530

    adding serializers

--- mysite/assignment/urls.py
+++ mysite/assignment/urls.py
@@ -1,12 +1,20 @@
-from django.urls import path
+from django.urls import path, include
 from assignment import views
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+router.register(r'classtaken', views.ClassTakenViewSet)
+
 urlpatterns = [
+    path('rest-api', include(router.urls)),
     path('index', views.index , name='index'),
     # url(r'^login/$', auth_views.LoginView, name='login'),
     # url(r'^logout/$', auth_views.LogoutView, name='logout'),
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^login/$', views.login, name='login'),
+    path('details/<int:pk>/',views.details,name='details'),
 ]
